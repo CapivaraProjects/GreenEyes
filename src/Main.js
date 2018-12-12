bb218ee7b2de18a78c372a150b33342ba1556087
Wendel Hime <wendelhime@hotmail.com>
2018-12-12 02:41:32 -0200

    adding queued functions

--- src/Main.js
+++ src/Main.js
@@ -6,17 +6,51 @@ import User from './scenes/User';
 import About from './scenes/About';
 import AnalisysController from './scenes/AnalisysController';
 import TermsOfUse from './scenes/TermsOfUse';
+import queueFactory from 'react-native-queue'
+import { db } from './index'
+import { getAnalisysById } from './api/AnalisysApi'
 export default class Main extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      queue: null
+    }
+    this.init()
   }
-    static navigationOptions = {
-        title: 'Main',
-    };
-    render() {
-      console.log(this.props.navigation.state);
-      return (
-        <AppTabNavigator screenProps={{token: this.props.navigation.state.params.token, user: this.props.navigation.state.user}}/>
+
+  async init() {
+    const queue = await queueFactory()
+    queue.addWorker('sync_analysis', async (id, payload) => {
+      console.log('Receiveid analysis: ' + payload)
+      for (analysis in user.analysis) {
+        a = getAnalisysById(analysis.id).response.response
+        await new Promise((resolve) => {
+          queue.createJob('sync_analysis_results', {
+            'results': a.analysis_result
+          })
+          resolve()
+        })
+        db.transaction(tx => {
+          tx.executeSql('')
+        })
+      }
+    })
+    queue.addWorker('sync_analysis_results', async (id, payload) => {
+      console.log('Receive analysis: ' + payload)
+    })
+    queue.start()
+    this.setState({
+      queue
+    });
+  }
+
+  static navigationOptions = {
+    title: 'Main',
+  };
+  render() {
+    console.log(this.props.navigation.state);
+    return (
+      <AppTabNavigator screenProps={{token: this.props.navigation.state.params.token, user: this.props.navigation.state.user}}/>
     );
   }
 }
